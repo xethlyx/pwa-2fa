31213ab89d1836eb6b7c3d6a2502a9eeb4e60ff1
xethlyx <46338199+xethlyx@users.noreply.github.com>
2021-03-01 02:20:15 -0500

    Modify name and theme color of PWA

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/pwa-2fa/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/pwa-2fa/' : '/',
+	pwa: {
+		name: 'PWA-2FA',
+		themeColor: '#121212'
+	}
 };
