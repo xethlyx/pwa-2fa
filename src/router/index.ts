b895b96888d3e0a0bacbbc1a554a384eec3b3c06
xethlyx <46338199+xethlyx@users.noreply.github.com>
2021-03-01 02:13:42 -0500

    Jekyll

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Settings from '../views/Settings.vue';
 
@@ -22,7 +22,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes
 });
 
